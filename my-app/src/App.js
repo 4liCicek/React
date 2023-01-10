203e453ed0d8de82f8919f35629323acce6c908e
Ali Ç <106672281+4liCicek@users.noreply.github.com>
2023-01-10 16:23:31 +0300

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,31 +10,41 @@ import Style2 from "./components/04-styles/style2";
 import Style3 from "./components/04-styles/style3";
 import Style4 from "./components/04-styles/style4";
 import Clock1 from "./components/05-clock1/clock1";
+import Greetings from "./components/06-props-1/greetings";
+import Products from "./components/07-props-2/products";
+import Clock2 from "./components/08-clock2/clock2";
+import Gallery from "./components/09-image/gallery";
+import Image from "./components/09-image/image";
+import ProfileCard from "./components/10-profile-card/profile-card";
 
 const App = () => {
+
+
   return (
     <div>
-
-      {/*   <HelloWorld />
-      <Jsx1 />
-      <Jsx2 />
-      <Jsx3 />
-      <Jsx4 />
-      <Jsx5 />
-       <Jsx6 />
-      <Style1 />
-      <Style2 />
-      <Style3 /> 
-      <Style4 />
-      <Clock1 />
-      */}
-
-      <Clock1 />
-
-
+      {/*         
+        <HelloWorld/>
+        <Jsx1/>
+        <Jsx2/>
+        <Jsx3/>
+        <Jsx4/>
+        <Jsx5/>
+        <Jsx6 />
+        <Style1/>
+        <Style2/>
+        <Style3/>
+        <Style4/>
+        <Clock1/>
+        <Greetings/>
+        <Products/>
+        <Clock2 textColor="#56ff33" bgColor="#0012C3" />
+<Image />
+<Gallery />
+<ProfileCard />
+ */}
+      <ProfileCard avatar="profile.jpg" name="Ali Gel" location="Turkey, Istanbul" shot="3" followers="23432" following="234" />
 
     </div>
-  )
-}
-
+  );
+};
 export default App;
