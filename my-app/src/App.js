787216f23c8eec3d8f78600b71eba1d8b67ba6dd
Ali Ç <106672281+4liCicek@users.noreply.github.com>
2023-01-12 08:30:49 +0300

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,7 @@ import Jsx3 from "./components/03-jsx/jsx3";
 import Jsx4 from "./components/03-jsx/jsx4";
 import Jsx5 from "./components/03-jsx/jsx5";
 import Jsx6 from "./components/03-jsx/jsx6";
+import Jsx7 from "./components/03-jsx/jsx7";
 import Style1 from "./components/04-styles/style1";
 import Style2 from "./components/04-styles/style2";
 import Style3 from "./components/04-styles/style3";
@@ -16,6 +17,15 @@ import Clock2 from "./components/08-clock2/clock2";
 import Gallery from "./components/09-image/gallery";
 import Image from "./components/09-image/image";
 import ProfileCard from "./components/10-profile-card/profile-card";
+import BootstrapDynamic from "./components/11-bootstrap/bootstrap-dynamic";
+import BootstrapStatic from "./components/11-bootstrap/bootstrap-static";
+import Icon from "./components/12-icons/icon";
+import Events from "./components/13-events/events";
+import Shop from "./components/14-products/shop";
+import State from "./components/15-usestate/state";
+import Stateless from "./components/15-usestate/stateless";
+import Counter from "./components/16-counter/counter";
+import Birthday from "./components/17-birthday/birthday";
 
 const App = () => {
 
@@ -30,6 +40,7 @@ const App = () => {
         <Jsx4/>
         <Jsx5/>
         <Jsx6 />
+        <Jsx7/>
         <Style1/>
         <Style2/>
         <Style3/>
@@ -37,12 +48,21 @@ const App = () => {
         <Clock1/>
         <Greetings/>
         <Products/>
-        <Clock2 textColor="#56ff33" bgColor="#0012C3" />
-<Image />
-<Gallery />
-<ProfileCard />
+        <Clock2 textColor="#56ff33" bgColor="#0012C3"/>
+        <Image/>
+        <Gallery/>
+        <ProfileCard avatar="profile.jpg" name="Ali Gel" location="Turkey, Istanbul" shot="3" followers="23432" following="234"/>
+        <BootstrapStatic/>
+        <BootstrapDynamic/>
+        <Icon/>
+        <Events/>
+        <Shop/>
+        <Stateless/>
+        <State/>
+        <Counter/>
  */}
-      <ProfileCard avatar="profile.jpg" name="Ali Gel" location="Turkey, Istanbul" shot="3" followers="23432" following="234" />
+
+      <Birthday />
 
     </div>
   );
