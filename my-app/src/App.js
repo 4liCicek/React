a80760fa731f285ad385ae80b222d8c328c0e74c
Ali Ç <106672281+4liCicek@users.noreply.github.com>
2023-01-18 08:25:58 +0300

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -26,8 +26,8 @@ import State from "./components/15-usestate/state";
 import Stateless from "./components/15-usestate/stateless";
 import Counter from "./components/16-counter/counter";
 import Birthday from "./components/17-birthday/birthday";
-import UseEffect from "./components/18-useeffect/useeffect1";
-import UseEffect2 from "./components/18-useeffect/useeffect2";
+// import UseEffect from "./components/18-useeffect/useeffect1";
+// import UseEffect2 from "./components/18-useeffect/useeffect2";
 import Clock3 from "./components/19-clock3/clock3";
 import CountryFilter from "./components/20-country-filter/country-filter";
 import Scroll from "./components/21-useref/scroll";
@@ -40,6 +40,8 @@ import Form1 from "./components/26-forms/form1";
 import Form2 from "./components/26-forms/form2";
 import Form3 from "./components/26-forms/form3";
 import Form4 from "./components/26-forms/form4";
+import Form5 from "./components/26-forms/form5";
+import Form6 from "./components/26-forms/form6";
 
 const App = () => {
 
@@ -88,9 +90,11 @@ const App = () => {
         <Form1/>
         <Form2/>
         <Form3/>
+        <Form4/>
+        <Form5/>
  */}
         
-        <Form4/>
+        <Form6/>
         
         
     </div>
