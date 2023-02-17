6d46625ff8561177c666b909e72cad1932de74a6
Ali Ç <106672281+4liCicek@users.noreply.github.com>
2023-02-17 11:45:37 +0300

    Update index.js

--- my-app/src/store/index.js
+++ my-app/src/store/index.js
@@ -1,1 +1,17 @@
 
+import { createContext } from "react";
+
+
+// Boş bir merkezi state oluşturuldu
+const StoreContext = createContext();
+
+
+// Merkezi state dışarıya açıldı
+export default StoreContext;
+
+
+
+
+
+
+
